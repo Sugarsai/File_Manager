876c8ab1d1839d06165602b15a5772eae33dee11
Sugarsai <soashraf29@gmail.com>
2024-04-29 06:32:49 +0300

    Push make.c

---
+++ core/make.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+#define COMMAND_LENGTH 100
+#define NAME_LENGTH 50
+
+void createFile()
+{
+    char filename[NAME_LENGTH];
+
+    printf("Enter File name: ");
+    fgets(filename, NAME_LENGTH, stdin);
+
+    char command[COMMAND_LENGTH];
+    snprintf(command, COMMAND_LENGTH, "touch \"%s\"", filename);
+
+    int status = system(command);
+
+    if (status != -1)
+    {
+        printf("File Created Successfully\n\n\n");
+    }
+    else
+    {
+        printf("Failed Creating File\n");
+        printf("Try Again ...\n");
+        createFile();
+    }
+}
+
+void createDirectory()
+{
+    char dirname[NAME_LENGTH];
+
+    printf("Enter Directory name: ");
+    fgets(dirname, NAME_LENGTH, stdin);
+
+    char command[COMMAND_LENGTH];
+    snprintf(command, COMMAND_LENGTH, "mkdir -p \"%s\"", dirname);
+
+    int status = system(command);
+
+    if (status != -1)
+    {
+        printf("Directory Created Successfully\n\n\n");
+    }
+    else
+    {
+        printf("Failed Creating Directory\n");
+        printf("Try Again ...\n");
+        createDirectory();
+    }
+}
